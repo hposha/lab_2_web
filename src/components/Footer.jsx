63ea7347ecbfde3389649e091e6f78e94bd29ea7
hposha <hpogta@gmail.com>
2026-03-19 01:01:02 +0200

    0.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-slate-950 p-6 text-center border-t border-slate-800">
       <p className="text-xs text-slate-500 uppercase tracking-widest">
-        &copy; 2026 Andrii Andrievsky. Лабораторна робота №3.
+        &copy; 2026 Andrii Andrievsky.
       </p>
     </footer>
   );
